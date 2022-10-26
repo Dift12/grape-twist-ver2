084577e1eb6ef7e3df45df3e655f30db6c4cf31a
Dift12 <diftbanda@gmail.comm>
2022-10-26 17:51:37 -0300

    Added event generator and style determining function

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { homeStyleTags } from "./homepage/homepage.js";
 import { aboutStyleTags } from "./about/about.js";
 
 import generateHomePage from "./homepage/homepage.js";
+import generateContactPage from "./contact/contactpage.js";
 
 coverStyleTags.turnOn();
 generateCover();
@@ -25,25 +26,66 @@ button.onclick= () => {
     siteLoadStyleTags.turnOn();
     siteload();
 
-    /*
-    mainExist=document.querySelector("main");
+    generateHomePage();      /*Default generated page*/ 
+    homeStyleTags.turnOn();
 
-    const links=document.querySelectorAll("nav a");
-
-  */
+    generateEvents();
 
     
 
-   //  aboutStyleTags.turnOn();
-    //generateAboutPage();
-    //homeStyleTags.turnOn();
-    //generateHomePage();
+    
+
+  
+}
+
+
+function generateEvents(){
+  const main=document.querySelector("main");
+  const nav=document.querySelectorAll("nav a");
+
+  nav.forEach(link=>link.addEventListener("click",(e)=>{
+
+    let value=e.target.textContent;
+
+
+    if(value=="home"){
+      main.innerHTML="";
+      determineStyle(value);
+      generateHomePage();
+    } else if(value=="about"){
+      main.innerHTML="";
+      determineStyle(value);
+      generateAboutPage()
+    } else {
+      main.innerHTML="";
+      determineStyle(value);
+      generateContactPage()
+    }
+  }))
+
+
 }
 
 
 
+/*Prevents lazy loaded style tags to stack up, loading only the needed one*/;
 
+function determineStyle(value){
+  switch(value){
+    case "home":
+      homeStyleTags.turnOn();
+      aboutStyleTags.turnOff();
+      break;
 
+    case "about":
+      homeStyleTags.turnOff();
+      aboutStyleTags.turnOn();
+      break;
 
+    case "contact":
+      homeStyleTags.turnOff();
+      aboutStyleTags.turnOff();
 
-
+    
+  }
+}
